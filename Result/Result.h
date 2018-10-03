1dfccf5d47a46671188668d31ce21416cf7bbc62
Anton Vodolazkyi <vodolazky@me.com>
2018-10-03 15:13:53 +0300

    Initial Commit

---
+++ Result/Result.h
@@ -0,0 +1,19 @@
+//
+//  Result.h
+//  Result
+//
+//  Created by Vodolazkyi Anton on 10/3/18.
+//  Copyright © 2018 Yalantis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Result.
+FOUNDATION_EXPORT double ResultVersionNumber;
+
+//! Project version string for Result.
+FOUNDATION_EXPORT const unsigned char ResultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Result/PublicHeader.h>
+
+
